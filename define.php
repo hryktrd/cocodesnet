3591eb3bb913209465ed4ac4f25b9faa5b79bb9f
hissan1983 <hissan1983@gmail.com>
2013-11-14 13:27:45 +0900

    セキュリティ上の問題でGitHubから削除

--- define.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('RDS_HOSTNAME'	, 'cocodesnetdb.catq2czaskqh.ap-northeast-1.rds.amazonaws.com');
-define('RDS_PORT'		, '3306');
-define('RDS_DB_NAME'	, 'cocodesnetdb');
-define('RDS_USERNAME'	, 'terada');
-define('RDS_PASSWORD'	, 'msql8399hytd');
-
